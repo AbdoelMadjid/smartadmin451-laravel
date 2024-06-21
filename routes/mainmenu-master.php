1a661a87c73d13f2fbe83c226d43ac2f24ec7684
AbdoelMadjid <abdulmadjid.mpd@gmail.com>
2024-06-21 12:44:51 +0700

    Update 21 June 2024 12.44

--- routes/mainmenu-master.php
+++ routes/mainmenu-master.php
@@ -30,3 +30,22 @@ Route::get('/tools_impor_data_master', [MasterController::class, 'tools_impor_da
 Route::get('/tools_ekspor_data_master', [MasterController::class, 'tools_ekspor_data_master'])->name('tools_ekspor_data_master');
 Route::get('/tools_backup_database', [MasterController::class, 'tools_backup_database'])->name('tools_backup_database');
 Route::get('/tools_data_login', [MasterController::class, 'tools_data_login'])->name('tools_data_login');
+
+//akademik
+Route::get('/akademik_indentitas_sekolah', [MasterController::class, 'akademik_indentitas_sekolah'])->name('akademik_indentitas_sekolah');
+Route::get('/akademik_tenaga_pendidik', [MasterController::class, 'akademik_tenaga_pendidik'])->name('akademik_tenaga_pendidik');
+Route::get('/akademik_paket_keahlian', [MasterController::class, 'akademik_paket_keahlian'])->name('akademik_paket_keahlian');
+Route::get('/akademik_mata_pelajaran', [MasterController::class, 'akademik_mata_pelajaran'])->name('akademik_mata_pelajaran');
+Route::get('/akademik_capaian_pembelajaran', [MasterController::class, 'akademik_capaian_pembelajaran'])->name('akademik_capaian_pembelajaran');
+Route::get('/akademik_kelas_walikelas', [MasterController::class, 'akademik_kelas_walikelas'])->name('akademik_kelas_walikelas');
+Route::get('/akademik_peserta_didik', [MasterController::class, 'akademik_peserta_didik'])->name('akademik_peserta_didik');
+
+//kurikulum
+Route::get('/kurikulum_versi', [MasterController::class, 'kurikulum_versi'])->name('kurikulum_versi');
+Route::get('/kurikulum_pengumuman', [MasterController::class, 'kurikulum_pengumuman'])->name('kurikulum_pengumuman');
+Route::get('/kurikulum_perakat_ujian', [MasterController::class, 'kurikulum_perakat_ujian'])->name('kurikulum_perakat_ujian');
+Route::get('/kurikulum_proses_kbm_perkelas', [MasterController::class, 'kurikulum_proses_kbm_perkelas'])->name('kurikulum_proses_kbm_perkelas');
+Route::get('/kurikulum_proses_kbm_perguru', [MasterController::class, 'kurikulum_proses_kbm_perguru'])->name('kurikulum_proses_kbm_perguru');
+Route::get('/kurikulum_proses_kbm_remedial', [MasterController::class, 'kurikulum_proses_kbm_remedial'])->name('kurikulum_proses_kbm_remedial');
+Route::get('/kurikulum_cetak_rapor', [MasterController::class, 'kurikulum_cetak_rapor'])->name('kurikulum_cetak_rapor');
+Route::get('/kurikulum_transkrip_nilai', [MasterController::class, 'kurikulum_transkrip_nilai'])->name('kurikulum_transkrip_nilai');
