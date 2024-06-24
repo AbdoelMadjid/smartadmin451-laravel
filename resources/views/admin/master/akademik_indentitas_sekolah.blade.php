488648056c80afbadeafcaa86fa08263335f93a0
AbdoelMadjid <abdulmadjid.mpd@gmail.com>
2024-06-24 11:00:54 +0700

    Update 24 June 2024

--- resources/views/admin/master/akademik_indentitas_sekolah.blade.php
+++ resources/views/admin/master/akademik_indentitas_sekolah.blade.php
@@ -19,14 +19,16 @@
             ])
             @endcomponent
         </div>
-        <x-col :size1="4" :size2="4" :size3="4">
+        <x-col :size1="4" :size2="3" :size3="5">
             <x-slot name='content1'>
-                <div class="card border mb-g">
-                    <!-- notice the additions of utility paddings and display properties on .card-header -->
+                <div class="card mb-g shadow-5">
                     <div class="card-header pr-3 d-flex align-items-center flex-wrap">
                         <!-- we wrap header title inside a span tag with utility padding -->
-                        <div class="card-title">Dropdowns</div>
-                        <button class="btn btn-icon btn-xs ml-auto fs-xl" data-toggle="dropdown">
+                        <div class="card-title mb-0 fs-xl fw-700"> <i class="fal fa-university color-primary-700"></i>
+                            Identitas Sekolah
+                        </div>
+                        <x-button color="primary" icon="edit" size="xs" pills :label="__('Edit')" class="ml-auto" />
+                        {{-- <button class="btn btn-icon btn-xs ml-auto fs-xl" data-toggle="dropdown">
                             <i class="fal fa-ellipsis-v"></i>
                         </button>
                         <div class="dropdown-menu dropdown-menu-animated dropdown-menu-right">
@@ -40,28 +42,109 @@
                             <button class="dropdown-item">
                                 Refresh
                             </button>
-                        </div>
+                        </div> --}}
                     </div>
-                    <div class="card-body">
-                        <p class="card-text">With supporting text below as a natural lead-in to
-                            additional content.</p>
+                    <div class="table-responsive m-4">
+                        <table class="table table-clean table-sm align-self-end"
+                            style='margin: 0 auto;width:100%;border-collapse:collapse;'>
+                            <tbody>
+                                <tr>
+                                    <td>Status</td>
+                                    <td>:</td>
+                                    <td>Negeri</td>
+                                </tr>
+                                <tr>
+                                    <td>Nama Sekolah</td>
+                                    <td>:</td>
+                                    <td>SMKN 1 Kadipaten</td>
+                                </tr>
+                                <tr>
+                                    <td>Status</td>
+                                    <td>:</td>
+                                    <td>Negeri</td>
+                                </tr>
+                                <tr>
+                                    <td>Status</td>
+                                    <td>:</td>
+                                    <td>Negeri</td>
+                                </tr>
+                                <tr>
+                                    <td>Status</td>
+                                    <td>:</td>
+                                    <td>Negeri</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </x-slot>
             <x-slot name='content2'>
-                <div class="card m-auto border shadow-5">
-                    <div class="card-header fw-700">
-                        Featured
+                <div class="card mb-g shadow-5">
+                    <div class="card-header pr-3 d-flex align-items-center flex-wrap">
+                        <!-- we wrap header title inside a span tag with utility padding -->
+                        <div class="card-title mb-0 fs-xl fw-700"> <i class="fal fa-calendar-alt color-primary-700"></i>
+                            Tahun Ajaran
+                        </div>
+                        <x-button color="primary" size="xs" pills :label="__('Opsion')" class="ml-auto" />
+                        {{-- <button class="btn btn-icon btn-xs ml-auto fs-xl" data-toggle="dropdown">
+                            <i class="fal fa-ellipsis-v"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-animated dropdown-menu-right">
+                            <button class="dropdown-item">
+                                Option 1
+                            </button>
+                            <button class="dropdown-item">
+                                Option 2
+                            </button>
+                            <div class="dropdown-divider m-0"></div>
+                            <button class="dropdown-item">
+                                Refresh
+                            </button>
+                        </div> --}}
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional
-                            content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                    <div class="row row-grid no-gutters">
+                        <div class="col-12">
+                            <table class="table table-sm table-bordered table-striped table-hover m-0">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Tahun Ajaran</th>
+                                        <th>Aktif</th>
+                                        <th>Ganjil</th>
+                                        <th>Genap</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>2021-2022</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>2022-2023</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>2025-2024</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div class="p-3">
+
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-footer text-muted py-2">
-                        2 days ago
-                    </div>
+
                 </div>
             </x-slot>
             <x-slot name='content3'>
@@ -71,16 +154,96 @@
                             <i class="fal fa-user-tie"></i> Kepala Sekolah
                         </h2>
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional
-                            content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                    <div class="card-footer text-muted py-2">
-                        2 days ago
+                    <div class="row row-grid no-gutters">
+                        <div class="col-12">
+                            <table class="table table-sm table-bordered table-striped table-hover m-0">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Tahun Ajaran</th>
+                                        <th>Semester</th>
+                                        <th>Nama Lengkap</th>
+                                        <th>Aktif</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>2021-2022</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>2022-2023</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>2025-2024</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div class="p-3">
+
+                            </div>
+                        </div>
                     </div>
                 </div>
+            </x-slot>
+        </x-col>
+
+        <x-col :size1="4" :size2="4" :size3="4">
+            <x-slot name='content1'>
+                <div class="card mb-g shadow-5">
+                    <div class="card-header pr-3 d-flex align-items-center flex-wrap">
+                        <!-- we wrap header title inside a span tag with utility padding -->
+                        <div class="card-title mb-0 fs-xl fw-700"> <i class="fal fa-camera color-primary-700"></i>
+                            Logo Sekolah
+                        </div>
+                        <x-button color="primary" size="xs" icon="upload" pills :label="__('Upload')" class="ml-auto" />
+                        {{-- <button class="btn btn-icon btn-xs ml-auto fs-xl" data-toggle="dropdown">
+                            <i class="fal fa-ellipsis-v"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-animated dropdown-menu-right">
+                            <button class="dropdown-item">
+                                Option 1
+                            </button>
+                            <button class="dropdown-item">
+                                Option 2
+                            </button>
+                            <div class="dropdown-divider m-0"></div>
+                            <button class="dropdown-item">
+                                Refresh
+                            </button>
+                        </div> --}}
+                    </div>
+                    <div class="align-items-center mt-5 mb-5">
+                        <div class='icon-stack display-1 flex-shrink-0 d-block m-auto'>
+                            <i class="fal fa-circle icon-stack-4x opacity-100 color-primary-400"></i>
+                            <i class="fas fa-download icon-stack-1x opacity-100 color-primary-500"></i>
+                        </div>
+                        <div class="p-1 text-center">
+                            <strong class="fw-900 fs-xxl color-primary-400">
+                                EKSPOR DATA MASTER
+                            </strong>
+                            <br>
+                            Laporan Capaian Kompetensi Siswa
+                        </div>
+                    </div>
+                </div>
+            </x-slot>
+            <x-slot name='content2'>
+
+            </x-slot>
+            <x-slot name='content3'>
+
             </x-slot>
         </x-col>
     </main>
